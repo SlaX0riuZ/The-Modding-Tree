4752aac1fe5ffa2e791e5ac701701f776f72ec82
SlaX0riuZ <slaxoriuz@gmail.com>
2023-01-02 23:55:37 -0600

    Update layers.js
    failure

--- js/layers.js
+++ js/layers.js
@@ -14,7 +14,6 @@ addLayer("1a", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        if (hasUpgrade('p', 11)) gain = gain.times(2)
         mult = new Decimal(1)
         return mult
     },
