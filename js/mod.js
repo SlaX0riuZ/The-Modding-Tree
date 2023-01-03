81aa0f4bf61e7ea8c5c1025974f1a838da8d9f34
SlaX0riuZ <slaxoriuz@gmail.com>
2023-01-02 23:29:53 -0600

    Update mod.js
    First update!

--- js/mod.js
+++ js/mod.js
@@ -1,26 +1,26 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Slax Tree",
+	id: "slaxmod",
+	author: "SlaXoriuZ",
+	pointsName: "cubes",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	offlineLimit: 5,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "New Beginnings",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- Initial Release.<br>
+		- Added layer 1.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
