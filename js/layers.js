200b87a828bc6ba0a4af026b9ae2bfd0b6c5efb2
SlaX0riuZ <slaxoriuz@gmail.com>
2023-01-02 23:42:51 -0600

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -6,10 +6,10 @@ addLayer("p", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDD13",
+    color: "#000000",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Prestige Cubes", // Name of prestige currency
-    baseResource: "cubes", // Name of resource prestige is based on
+    baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
